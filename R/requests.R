f08ba576c5a3075d15b1ff8bcb9e05073484edc0
Andrew B. Collier <andrew@exegetic.biz>
2016-08-04 17:55:38 +0200

    Fixed formatting of uber_requests_estimate().

--- R/requests.R
+++ R/requests.R
@@ -21,7 +21,9 @@ uber_requests <- function(start_latitude = NULL, start_longitude = NULL, end_lat
 uber_requests_estimate <- function(start_latitude = NULL, start_longitude = NULL, end_latitude = NULL, end_longitude = NULL) {
   response <- callAPI("requests/estimate", 1, method = "POST", params = parseParameters(environment()))
   #
-  response$price$fare_breakdown <- data.frame(bind_rows(response$price$fare_breakdown))
+  response$price <- with(response$price,
+                         cbind(bind_rows(fare_breakdown), surge_multiplier, currency_code)
+                         )
   #
   response
 }
