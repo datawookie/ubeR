9c0387e5a9ec32dfd96aca3dfbb95003b140d594
Andrew B. Collier <andrew@exegetic.biz>
2016-08-05 05:17:01 +0200

    Consolidated endpoints into a single file.

--- R/api.R
+++ R/api.R
@@ -1,3 +1,141 @@
+# PRODUCTS ------------------------------------------------------------------------------------------------------------
+
+#' https://developer.uber.com/docs/rides/api/v1-products
+#' @export
+products <- function(latitude = NA, longitude = NA, product_id = NA) {
+  if (any(is.na(c(latitude, longitude))) && is.na(product_id))
+    stop("Either both latitude and longitude or product_id must be specified.")
+  #
+  latitude = as.numeric(latitude)
+  longitude = as.numeric(longitude)
+  #
+  product_id = as.character(product_id)
+
+  if (!is.na(longitude) && (longitude > 180 || longitude < -180)) {
+    stop('Longitude must be in range [180,-180].')
+  }
+  if (!is.na(latitude) && (latitude > 90 || latitude < -90)) {
+    stop('Latitude must be in range [90.0,-90.0].')
+  }
+
+  if (is.na(product_id)) {
+    rides = callAPI("products", 1, buildArguments(latitude = latitude, longitude = longitude))$products
+  } else {
+    rides = list(callAPI(paste("products", product_id, sep = "/"), 1))
+  }
+
+  rides = lapply(rides, function(ride) {
+    data.frame(t(unlist(ride)), stringsAsFactors = FALSE)
+  })
+  bind_rows(rides)
+}
+
+# ESTIMATES -----------------------------------------------------------------------------------------------------------
+
+#' https://developer.uber.com/docs/rides/api/v1-estimates-price
+#' @export
+uber_estimate_price <- function(start_latitude, start_longitude, end_latitude, end_longitude, seat_count = NULL) {
+  estimates = callAPI("estimates/price", 1, method = "GET", params = parseParameters(environment()))
+
+  bind_rows(lapply(estimates$prices, function(estimate) {
+    data.frame(t(unlist(estimate)), stringsAsFactors = FALSE)
+  }))
+}
+
+#' https://developer.uber.com/docs/rides/api/v1-estimates-time
+#' @export
+uber_estimate_time <- function(start_latitude, start_longitude, product_id = NULL) {
+  estimates = callAPI("estimates/time", 1, method = "GET", params = parseParameters(environment()))
+
+  bind_rows(lapply(estimates$times, function(estimate) {
+    data.frame(t(unlist(estimate)), stringsAsFactors = FALSE)
+  }))
+}
+
+# HISTORY -------------------------------------------------------------------------------------------------------------
+
+#' https://developer.uber.com/docs/rides/api/v12-history
+#'
+#' Requires an OAuth 2.0 token with the history or history_lite scope.
+#'
+#' @export
+uber_history <- function(limit = 5, offset = 0) {
+
+  print(parseParameters(environment()))
+  data <- callAPI("history", 1.2,  method = "GET", params = parseParameters(environment()))
+  history.df <- data$history
+  history.df <- as.data.frame(do.call(rbind, history))
+  history.df <- as.data.frame(lapply(history.df, unlist))
+  history.df[, 3:5] <- lapply(history.df[, 3:5], as.POSIXct, origin = "1970-01-01")
+
+  final <- list(history = history.df, limit = data$limit,
+                offset = data$offset)
+  return(final)
+}
+
+# ME ------------------------------------------------------------------------------------------------------------------
+
+#' https://developer.uber.com/docs/rides/api/v1-me
+#'
+#' Requires an OAuth 2.0 token with the profile scope.
+#'
+#' @export
+uber_me <- function() {
+  callAPI("me", 1)
+}
+
+# REQUESTS ------------------------------------------------------------------------------------------------------------
+
+#' https://developer.uber.com/docs/rides/api/v1-requests
+#'
+#' Requires an OAuth 2.0 token with the request scope.
+#'
+#' @param start_latitude
+#' @param start_longitude
+#' @param end_latitude
+#' @param end_longitude
+#' @param start_address
+#' @param end_address
+#' @export
+uber_requests <- function(start_latitude = NULL, start_longitude = NULL, end_latitude = NULL, end_longitude = NULL, start_address = NULL, end_address = NULL) {
+  callAPI("requests", 1, method = "POST", params = parseParameters(environment()))
+}
+
+#' https://developer.uber.com/docs/rides/api/v1-requests-estimate
+#'
+#' Requires an OAuth 2.0 token with the request scope
+#'
+#' @export
+uber_requests_estimate <- function(start_latitude = NULL, start_longitude = NULL, end_latitude = NULL, end_longitude = NULL) {
+  response <- callAPI("requests/estimate", 1, method = "POST", params = parseParameters(environment()))
+  #
+  response$price <- with(response$price,
+                         cbind(bind_rows(fare_breakdown), surge_multiplier, currency_code)
+  )
+  #
+  response
+}
+
+#' https://developer.uber.com/docs/rides/api/v1-requests-current
+#'
+#' Requires an OAuth 2.0 token with the all_trips or request scope
+#'
+#' @export
+uber_requests_current <- function() {
+  callAPI("requests/current", 1)
+}
+
+#' https://developer.uber.com/docs/rides/api/v1-requests-current-delete
+#'
+#' Requires an OAuth 2.0 token with the request scope
+#'
+#' @export
+uber_requests_current_delete <- function() {
+  callAPI("requests/current", 1, method = "DELETE")
+}
+
+# PLACES --------------------------------------------------------------------------------------------------------------
+
 #' It's possible that a home location will need to be set via the Uber application before this will work.
 #'
 #' https://developer.uber.com/docs/rides/api/v1-places-get
@@ -20,4 +158,20 @@ uber_places_get <- function(place_id = c("home", "work")) {
 uber_places_put <- function(place_id = c("home", "work"), address) {
   place_id = place_id[1]
   callAPI(paste("places", place_id, sep = "/"), 1, method = "PUT", params = list(address = address))
+}
+
+# PAYMENT -------------------------------------------------------------------------------------------------------------
+
+#' https://developer.uber.com/docs/rides/api/v1-payment-methods
+#'
+#' Requires an OAuth 2.0 token with the request scope.
+#'
+#' @export
+uber_payment_methods <- function() {
+  newlist <- callAPI("payment-methods", 1)
+  payment <- newlist$payment_methods
+  payment <- lapply(payment, nullToNA)
+  df <- as.data.frame(do.call(rbind, payment))
+  df <- as.data.frame(lapply(df, unlist))
+  return(df)
 }
--- R/me.R
+++
@@ -1,8 +0,0 @@
-#' https://developer.uber.com/docs/rides/api/v1-me
-#'
-#' Requires an OAuth 2.0 token with the profile scope.
-#'
-#' @export
-uber_me <- function() {
-  callAPI("me", 1)
-}
