cda7fc5fe4e830807a2180a6bd220f2070b14eb4
Andrew B. Collier <andrew@exegetic.biz>
2016-08-05 13:00:46 +0200

    Completed documentation for uber_oauth().

--- R/authorise.R
+++ R/authorise.R
@@ -18,21 +18,35 @@ uber_get_serverid <- function() {
 
 # OAUTH2 --------------------------------------------------------------------------------------------------------------
 
-#' It might be worthwhile considering saving a default token as is done for rdrop2 package.
+# It might be worthwhile considering saving a default token as is done for rdrop2 package.
+
+#' Authenticate with the Uber API
 #'
+#' This function wraps the process of OAuth2.0 authentication with the Uber API. This must be done before further interactions can take place.
+#'
+#' If \code{cache = TRUE} then the authentication token will be stored in a \code{.httr-oauth} file.
+#'
+#' @param client_key    The client key provided by Uber.
+#' @param client_secret The client secreat provided by Uber.
+#' @param cache         Whether or not to cache authentication token.
+#' @seealso \code{\link[httr]{oauth_app}}.
+#' @examples
+#' \dontrun{
+#' uber_oauth("ReTZRlEGNnzkitsn-A23MiXbnMNzdQf8", "NpWYGY8W7cv63tfM2neciVpjZOAF_wx1GHRG94A2")
+#' }
 #' @export
-uber_oauth = function(client_key, client_secret) {
+uber_oauth <- function(client_key, client_secret, cache = FALSE) {
   endpoint <- httr::oauth_endpoint(
     authorize = "https://login.uber.com/oauth/v2/authorize",
     access    = "https://login.uber.com/oauth/v2/token"
   )
-  app <- httr::oauth_app("uber", key = client_key, secret = client_secret)
 
   # Sys.setenv("HTTR_SERVER_PORT" = "1410/")
   #
-  # scope = c("profile", "request", "history_lite", "places", "history", "ride_widgets")
   scope = c("profile", "history", "places", "request")
-  token <- httr::oauth2.0_token(endpoint, app, scope = scope, cache = FALSE)
+  token <- httr::oauth2.0_token(endpoint,
+                                httr::oauth_app("uber", key = client_key, secret = client_secret),
+                                scope = scope, cache = cache)
 
   assign("oauth_token", token, envir = auth_cache)
 }
@@ -41,7 +55,6 @@ has_oauth_token <- function() {
   exists("oauth_token", envir = auth_cache)
 }
 
-#' @export
 get_oauth_token <- function() {
   if (!has_oauth_token()) {
     stop("This session doesn't yet have OAuth2.0 authentication.")
