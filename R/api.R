40a8354eb39e411b300af61716fef2c3651b3bc2
Andrew B. Collier <andrew@exegetic.biz>
2016-08-05 06:09:52 +0200

    Applied changes to uber_estimate_price() and uber_estimate_time().

--- R/api.R
+++ R/api.R
@@ -36,20 +36,16 @@ products <- function(latitude = NA, longitude = NA, product_id = NA) {
 #' @export
 uber_estimate_price <- function(start_latitude, start_longitude, end_latitude, end_longitude, seat_count = NULL) {
   estimates = callAPI("estimates/price", 1, method = "GET", params = parseParameters(environment()))
-
-  bind_rows(lapply(estimates$prices, function(estimate) {
-    data.frame(t(unlist(estimate)), stringsAsFactors = FALSE)
-  }))
+  #
+  estimates$prices
 }
 
 #' https://developer.uber.com/docs/rides/api/v1-estimates-time
 #' @export
 uber_estimate_time <- function(start_latitude, start_longitude, product_id = NULL) {
   estimates = callAPI("estimates/time", 1, method = "GET", params = parseParameters(environment()))
-
-  bind_rows(lapply(estimates$times, function(estimate) {
-    data.frame(t(unlist(estimate)), stringsAsFactors = FALSE)
-  }))
+  #
+  estimates$times
 }
 
 # HISTORY -------------------------------------------------------------------------------------------------------------
