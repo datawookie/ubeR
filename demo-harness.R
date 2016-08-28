f894564cf332df3ac2a77f67f0898432c2433a49
Andrew B. Collier <andrew@exegetic.biz>
2016-08-28 09:25:51 +0200

    Changes to demonstration script for blog post. Also renamed.

--- demo-harness.R
+++ demo-harness.R
@@ -1,9 +1,4 @@
-source("R/cache.R")
-source("R/authorise.R")
-source("R/httr.R")
-source("R/utils.R")
-source("R/api.R")
-
+library(ubeR)
 library(dplyr)
 
 # You'll need to set up these environment variables first.
@@ -19,11 +14,12 @@ UBER_CLIENTSECRET = Sys.getenv("UBER_CLIENTSECRET")
 
 uber_oauth(UBER_CLIENTID, UBER_CLIENTSECRET, TRUE)
 
-uber_me()
+(identity <- uber_me())
+uber_payment_methods()
 
 uber_history()
 
-uber_products(latitude = -33.925278, longitude = 18.423889)
+cars <- uber_products(latitude = -33.925278, longitude = 18.423889)
 uber_products(product_id = "91901472-f30d-4614-8ba7-9fcc937cebf5")
 
 uber_places_get()
@@ -35,19 +31,17 @@ uber_places_get("work")
 # uber_places_put("home", "115 St Andrews Dr, Durban North, 4051, South Africa")
 # uber_places_get()
 
-uber_payment_methods()
+estimate <- uber_requests_estimate(start_latitude = -33.899656, start_longitude = 18.407663,
+                                   end_latitude = -33.927443, end_longitude = 18.457557)
 
-uber_requests_estimate(start_latitude = 37.761492, start_longitude = -122.423941,
-                       end_latitude = 37.775393, end_longitude = -122.417546)
+estimate <- uber_estimate_price(start_latitude = -33.899656, start_longitude = 18.407663,
+                    end_latitude = -33.927443, end_longitude = 18.457557)
 
-uber_requests(start_address = "37 Beach Road, Mouille Point, Cape Town",
-              end_address = "100 St Georges Mall, Cape Town City Centre, Cape Town, 8001")
+uber_estimate_time(start_latitude = -33.899656, start_longitude = 18.407663)
+
+ride <- uber_requests(start_address = "37 Beach Road, Mouille Point, Cape Town",
+                      end_address = "100 St Georges Mall, Cape Town City Centre, Cape Town, 8001")
 
 uber_requests_current()
 uber_requests_current_delete()
 uber_requests_current()
-
-uber_estimate_price(start_latitude = 37.761492, start_longitude = -122.423941,
-               end_latitude = 37.775393, end_longitude = -122.417546)
-
-uber_estimate_time(start_latitude = 37.761492, start_longitude = -122.423941)
