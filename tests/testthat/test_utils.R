45972e0a5913446445ad076c9474527871367b34
Andrew B. Collier <andrew@exegetic.biz>
2016-10-03 12:40:44 +0200

    Added test which will fail.

--- tests/testthat/test_utils.R
+++ tests/testthat/test_utils.R
@@ -3,3 +3,8 @@ context("geocode")
 test_that("raise error on fail to geocode", {
   expect_error(geocode("The Old Biscuit Mill"), "Unable to find location 'The Old Biscuit Mill'.")
 })
+
+test_that("this will fail", {
+  print("this will fail!")
+  expect_equal(1, 0)
+})
